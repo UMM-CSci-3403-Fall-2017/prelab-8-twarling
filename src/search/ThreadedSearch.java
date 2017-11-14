ce6f03212c186ffb1b340e66e023581be240a903
twarling <warli010@morris.umn.edu>
2017-11-14 11:28:42 -0600

    Added functionality to ThreadedSearch and cleaned/commented code.

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -27,30 +27,38 @@ public class ThreadedSearch<T> implements Runnable {
   * You can assume that the list size is divisible by `numThreads`
   */
   public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
-    /*
-    * First construct an instance of the `Answer` inner class. This will
-    * be how the threads you're about to create will "communicate". They
-    * will all have access to this one shared instance of `Answer`, where
-    * they can update the `answer` field inside that instance.
-    *
-    * Then construct `numThreads` instances of this class (`ThreadedSearch`)
-    * using the 5 argument constructor for the class. You'll hand each of
-    * them the same `target`, `list`, and `answer`. What will be different
-    * about each instance is their `begin` and `end` values, which you'll
-    * use to give each instance the "job" of searching a different segment
-    * of the list. If, for example, the list has length 100 and you have
-    * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
-    * [50, 75), and [75, 100) as their sections to search.
-    *
-    * You then construct `numThreads`, each of which is given a different
-    * instance of this class as its `Runnable`. Then start each of those
-    * threads, wait for them to all terminate, and then return the answer
-    * in the shared `Answer` instance.
-    */
-    return false;
+	
+	Answer answer = new Answer();
+	int subDiv = list.size() / numThreads;
+	
+	// Creates a thread array that all of the threads will be stored in
+	Thread[] threads = new Thread[numThreads];
+	
+	//Creates the specified number of threads in the thread array, and creates a new instance of ThreadedSearch for each individual thread
+	for(int i = 0; i < numThreads; i++){
+		threads[i] = new Thread (new ThreadedSearch(target, list, subDiv * i, subDiv * (i + 1), answer));
+		threads[i].start();
+	}
+	
+	// forces the program to wait for all of the threads to finish searching before moving on
+	for(int i = 0; i < numThreads; i++){
+		threads[i].join();
+	}
+	
+	
+    return answer.getAnswer();
   }
 
   public void run() {
+	  
+	  // Loops through the list within the given beginning and end locations, and checks if the value at i is equal to the number being looked for.
+	  // If the value at i is equal to the one being looked for, answer is set to true and the loop is ended. 
+	  for(int i = begin; i < end; i++){
+          if(list.get(i).equals(target)) {
+        	  answer.setAnswer(true);
+        	  return;
+          }
+      }
 
   }
 
@@ -69,3 +77,4 @@ public class ThreadedSearch<T> implements Runnable {
   }
 
 }
+
